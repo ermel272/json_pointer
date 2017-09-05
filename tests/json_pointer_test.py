643c145348a8bb41bb6c137108656c9aab01ad65
Chris Ermel <ermel272@gmail.com>
2017-09-04 21:10:40 -0400

    adding a hash test

--- tests/json_pointer_test.py
+++ tests/json_pointer_test.py
@@ -32,6 +32,10 @@ class JsonPointerTest(unittest.TestCase):
         self.assertTrue(self.pointer == JsonPointer('/foo/1'))
         self.assertFalse(self.pointer == JsonPointer('/foo'))
 
+    def test_hash(self):
+        self.assertTrue(self.pointer.__hash__() == JsonPointer('/foo/1').__hash__())
+        self.assertFalse(self.pointer.__hash__() == JsonPointer('/foo').__hash__())
+
     def test_init_validation(self):
         self.assertRaises(JsonPointerException, JsonPointer, 0)
         self.assertRaises(JsonPointerException, JsonPointer, 'invalid')
